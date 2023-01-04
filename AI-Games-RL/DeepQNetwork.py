fadb0697bc469c7bc06990588d1139e0fb3f77f6
lucylawr <lucylawr@hotmail.co.uk>
2023-01-04 21:09:15 +0000

    Deep Q Learning

---
+++ AI-Games-RL/DeepQNetwork.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+from collections import deque
+
+class DeepQNetwork(torch.nn.Module):
+    def __init__(self, env, learning_rate, kernel_size, conv_out_channels, 
+                 fc_out_features, seed):
+        torch.nn.Module.__init__(self)
+        torch.manual_seed(seed)
+
+        self.conv_layer = torch.nn.Conv2d(in_channels=env.state_shape[0], 
+                                          out_channels=conv_out_channels,
+                                          kernel_size=kernel_size, stride=1)
+
+        h = env.state_shape[1] - kernel_size + 1
+        w = env.state_shape[2] - kernel_size + 1
+
+        self.fc_layer = torch.nn.Linear(in_features=h * w * conv_out_channels, 
+                                        out_features=fc_out_features)
+        self.output_layer = torch.nn.Linear(in_features=fc_out_features, 
+                                            out_features=env.n_actions)
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+
+    def forward(self, x):
+        x = torch.tensor(x, dtype=torch.float)
+        
+        # TODO: 
+        x = F.relu(self.conv_layer(x))
+        x = F.relu(self.fc_layer(x))
+        return self.output_layer(x)
+
+    def train_step(self, transitions, gamma, tdqn):
+        states = np.array([transition[0] for transition in transitions])
+        actions = np.array([transition[1] for transition in transitions])
+        rewards = np.array([transition[2] for transition in transitions])
+        next_states = np.array([transition[3] for transition in transitions])
+        dones = np.array([transition[4] for transition in transitions])
+
+        q = self(states)
+        q = q.gather(1, torch.Tensor(actions).view(len(transitions), 1).long())
+        q = q.view(len(transitions))
+
+        with torch.no_grad():
+            next_q = tdqn(next_states).max(dim=1)[0] * (1 - dones)
+
+        target = torch.Tensor(rewards) + gamma * next_q
+
+        # TODO: the loss is the mean squared error between `q` and `target`
+        loss = np.square(np.subtract(target, q)).mean()
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()    
+        
+        
+class ReplayBuffer:
+    def __init__(self, buffer_size, random_state):
+        self.buffer = deque(maxlen=buffer_size)
+        self.random_state = random_state
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def append(self, transition):
+        self.buffer.append(transition)
+
+    def draw(self, batch_size):
+        # TODO:
+        return [self.buffer.pop() for i in range(batch_size)]
---
+++ AI-Games-RL/FrozenLakeImageWrapper.py
@@ -0,0 +1,44 @@
+import numpy as np
+import contextlib
+
+class FrozenLakeImageWrapper:
+    def __init__(self, env):
+        self.env = env
+        
+        lake = self.env.lake
+
+        self.n_actions = self.env.n_actions
+        self.state_shape = (4, lake.shape[0], lake.shape[1])
+
+        lake_image = [(lake == c).astype(float) for c in ['&', '#', '$']] # a list
+
+        self.state_image = {self.env.absorbing_state: 
+                            np.stack([np.zeros(lake.shape)] + lake_image)}
+        
+        # not sure if this is correct
+        for state in range(lake.size):
+            # TODO: 
+            self.state_image[state] = np.stack([np.zeros(lake.shape)] + lake_image)
+
+    def encode_state(self, state):
+        return self.state_image[state]
+
+    def decode_policy(self, dqn):
+        states = np.array([self.encode_state(s) for s in range(self.env.n_states)])
+        q = dqn(states).detach().numpy()  # torch.no_grad omitted to avoid import
+
+        policy = q.argmax(axis=1)
+        value = q.max(axis=1)
+
+        return policy, value
+
+    def reset(self):
+        return self.encode_state(self.env.reset())
+
+    def step(self, action):
+        state, reward, done = self.env.step(action)
+
+        return self.encode_state(state), reward, done
+
+    def render(self, policy=None, value=None):
+        self.env.render(policy, value)
